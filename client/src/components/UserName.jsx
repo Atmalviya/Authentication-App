a744e3bf29dce6d0c9693a405a74785563ac079f
Atul <atulmalviyav2@gmail.com>
2024-07-17 19:23:05 +0530

    Login component

---
+++ client/src/components/UserName.jsx
@@ -0,0 +1,62 @@
+import { Link } from "react-router-dom";
+import avater from "../assets/profile.png";
+import { Toaster } from "react-hot-toast";
+import { useFormik } from "formik";
+import styles from "../styles/Username.module.css";
+import { userNameValidator } from "../helper/validate.js";
+
+const UserName = () => {
+  const formik = useFormik({
+    initialValues: {
+      Username: "",
+    },
+    validate: userNameValidator,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async (values) => {
+      console.log(values);
+    },
+  });
+
+  return (
+    <div className="container mx-auto">
+      <Toaster position="top-center" reverseOrder={false}></Toaster>
+      <div className="flex items-center justify-center h-screen">
+        <div className={styles.glass}>
+          <div className="title flex flex-col items-center">
+            <h4 className="text-5xl font-bold">Hello</h4>
+            <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+              Explore more here by clciking below
+            </span>
+          </div>
+          <form className="py-1" onSubmit={formik.handleSubmit}>
+            <div className="profile flex justify-center py-4">
+              <img src={avater} alt="avatar" className={styles.profile_img} />
+            </div>
+            <div className="textbox flex flex-col items-center gap-6">
+              <input
+                type="text"
+                placeholder="Username"
+                className={styles.textbox}
+                {...formik.getFieldProps("Username")}
+              />
+              <button className={styles.btn} type="submit">
+                Let's Go
+              </button>
+            </div>
+            <div className="text-center py-4">
+              <span>
+                Not a Member?{" "}
+                <Link className="text-gray-500" to="/register">
+                  Register Now
+                </Link>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserName;
