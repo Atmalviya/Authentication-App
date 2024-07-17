71510109aae4689f3c19e7e5a6b2c360f803d89f
Atul <atulmalviyav2@gmail.com>
2024-07-17 21:43:02 +0530

    implementation of all routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import './index.css';
+
+import PageNotFound from './components/PageNotFound';
+import Password from './components/Password';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Register from './components/Register';
+import Reset from './components/Reset';
+import UserName from './components/UserName';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      React App
-    </>
-  )
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<UserName />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/password" element={<Password />} />
+          <Route path="/recovery" element={<Recovery />} />
+          <Route path="/reset" element={<Reset />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
 }
 
-export default App
+export default App;
