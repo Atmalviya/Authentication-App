ec99fa124df944acbfb5e88f94d369581ec84f17
Atul <atulmalviyav2@gmail.com>
2024-07-17 19:27:54 +0530

    tailwind configuration

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {
+      // color used in project
+      colors: {
+        primary: "#2B85FF",
+        secondary: "#EF863E",
+      },
+    },
+    plugins: [],
+  },
+};
