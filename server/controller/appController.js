66b88c4635614e1f5f8bfb1b30710736e8db41f9
Atul <atulmalviyav2@gmail.com>
2024-07-19 01:10:30 +0530

    routes implementations

--- server/controller/appController.js
+++ server/controller/appController.js
@@ -1,62 +1,190 @@
-//* Register Route 
-async function register(req, res) {
-    res.status(200).json("Register");
+const User = require("../model/User.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const otpGenerate = require("otp-generator");
+require("dotenv").config();
+
+//* verify user
+async function verifyUser(req, res, next) {
+  const { username } = req.method == "GET" ? req.query : req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  next();
 }
 
-//* Register Mail Route
-async function registerMail(req, res) {
-    res.status(200).json("RegisterMail");
-} 
+//* Register Route
+async function register(req, res) {
+  try {
+    const { username, email, password, profile } = req.body;
 
-//* authenticate Route
-async function authenticate(req, res) {
-    res.status(200).json("authenticate");
+    // Check if user email already exists
+    const user = await User.findOne({ email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ message: "User with this email already exists" });
+    }
+
+    // Check if username already exists
+    const userName = await User.findOne({ username });
+    if (userName) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+
+    // Hash password
+    let hashedPassword;
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      hashedPassword = await bcrypt.hash(password, salt);
+    }
+
+    // Create new user
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      profile: profile || "",
+    });
+
+    const userData = await newUser.save();
+    return res
+      .status(201)
+      .json({ message: "User registerd successfully", userData });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error", error });
+  }
 }
 
 //* Login Route
 async function login(req, res) {
-    res.status(200).json("Login");
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    user.password = undefined;
+
+    // Send response
+    return res
+      .status(200)
+      .json({ message: "Login successful", username, token });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+}
+
+//* Register Mail Route
+async function registerMail(req, res) {
+    
+}
+
+//* authenticate Route
+async function authenticate(req, res) {
+  res.status(200).json("authenticate");
 }
 
 //* getUser Route
 async function getUser(req, res) {
-    res.status(200).json("getUser route");
+  const { username } = req.params;
+  const user = await User.findOne({ username }).select("-password");
+  if (!user) return res.status(404).json({ message: "User not found" });
+  return res.status(201).json(user);
 }
 
 //* update user Route
 async function updateUser(req, res) {
-    res.status(200).json("updateUser route");
-} 
+  try {
+    const id = req.user.id;
+    if (!id) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const updatedUser = await User.findByIdAndUpdate(id, req.body).select(
+      "-password"
+    );
+    return res
+      .status(201)
+      .json({ msg: "User updated successfully", updatedUser });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error", error });
+  }
+}
 
 //* Generate OTP Route
 async function generateOTP(req, res) {
-    res.status(200).json("generateOTP route");
-} 
+  req.app.locals.OTP = otpGenerate.generate(6, {
+    lowerCaseAlphabets: false,
+    upperCaseAlphabets: false,
+    specialChars: false,
+  });
+  res.status(200).json({ otp: req.app.locals.OTP });
+}
 
 //* Verify OTP Route
 async function verifyOTP(req, res) {
-    res.status(200).json("verifyOTP route");
+  const { otp } = req.query;
+  if (parseInt(otp) == parseInt(req.app.locals.OTP)) {
+    req.app.locals.OTP = null;
+    req.app.locals.resetSession = true;
+    return res.status(201).json({ message: "OTP verified" });
+  }
+  return res.status(400).json({ message: "Invalid OTP" });
 }
 
 //* create reset session
 async function createResetSession(req, res) {
-    res.status(200).json("createResetSession route");
+  if (req.app.locals.resetSession) {
+    req.app.locals.resetSession = false;
+    return res.status(201).json({ message: "Reset session created" });
+  }
+  return res.status(400).json({ error: "session expired" });
 }
 
 //* reset password
 async function updatePassword(req, res) {
-    res.status(200).json("resetPassword route");
-}  
+  try {
+    if (!req.app.locals.resetSession)
+      return res.status(400).json({ error: "Session expired" });
+    const { username, password } = req.body;
+    try {
+      const user = await User.findOne({ username });
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      const hashedPassword = await bcrypt.hash(password, 10);
+      user.password = hashedPassword;
+      await user.save();
+      req.app.locals.resetSession = false;
+      return res.status(201).json({ message: "Password updated successfully" });
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  } catch (error) {
+    return res.status(401).json(error);
+  }
+}
 
 module.exports = {
-    register,
-    registerMail,
-    authenticate,
-    login,
-    getUser,
-    updateUser,
-    generateOTP,
-    verifyOTP,
-    createResetSession,
-    updatePassword
-}
+  verifyUser,
+  register,
+  registerMail,
+  authenticate,
+  login,
+  getUser,
+  updateUser,
+  generateOTP,
+  verifyOTP,
+  createResetSession,
+  updatePassword,
+};
--- server/router/routes.js
+++ server/router/routes.js
@@ -1,23 +1,22 @@
 const router = require("express").Router();
 const controller = require("../controller/appController");
+const {authMiddleware, localVar} = require("../middleware/authMiddleware");
+const {registerMail} = require("../controller/mailer");
 
 //* POST Routes
 router.route("/register").post(controller.register);
-
-router.route("/registerMail").post(controller.registerMail);
-
+router.route("/registerMail").post(registerMail);
 router.route("/authenticate").post(controller.authenticate);
-
 router.route("/login").post(controller.login);
 
 //* GET Routes
 router.route("/user/:username").get(controller.getUser);
-router.route("/generateOTP").get(controller.generateOTP);
-router.route("/verifyOTP").get(controller.verifyOTP);
+router.route("/generateOTP").get(controller.verifyUser, localVar, controller.generateOTP);
+router.route("/verifyOTP").get(controller.verifyUser,controller.verifyOTP);
 router.route("/createResetSession").get(controller.createResetSession);
 
 //* PUT Routes
-router.route("/updatePassword").put(controller.updatePassword);
-router.route("/updateUser").put(controller.updateUser);
+router.route("/updateUser").put(authMiddleware, controller.updateUser);
+router.route("/updatePassword").put(controller.verifyUser, controller.updatePassword);
 
 module.exports = router;
