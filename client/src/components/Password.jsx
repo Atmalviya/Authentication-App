c4aca52bc1ac3d06e84b76f6990774250d7112c4
Atul <atulmalviyav2@gmail.com>
2024-07-25 15:48:28 +0530

    password comp api added and toast notificaton added

--- client/src/components/Password.jsx
+++ client/src/components/Password.jsx
@@ -1,22 +1,44 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import avater from "../assets/profile.png";
-import { Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 import { useFormik } from "formik";
 import styles from "../styles/Username.module.css";
 import { passwordValidator } from "../helper/validate.js";
+import useFetch from "../Hooks/fetch.hook.js";
+import { useAuthStore } from "../store/store.js";
+import { loginUser } from "../helper/axios.js"; 
 
 const Password = () => {
+  const navigate = useNavigate();
+  const { username } = useAuthStore((state) => state.auth);
+  const [{ isLoading, apiData, serverError }] = useFetch(`user/${username}`);
+
   const formik = useFormik({
     initialValues: {
-      Password: '',
+      password: "",
     },
     validate: passwordValidator,
     validateOnBlur: false,
     validateOnChange: false,
     onSubmit: async (values) => {
-      console.log(values);
+      const loadingToast = toast.loading("Checking password...");
+      try {
+        const response = await loginUser({ username, password: values.password });
+        let { token } = response.data;
+        localStorage.setItem("token", token);
+        navigate("/profile");
+        toast.success("Login Successful...");
+      } catch (error) {
+        toast.error(error.message ); // Show error message from server
+      } finally {
+        toast.dismiss(loadingToast);
+        
+      }
     },
   });
+
+  if (isLoading) {return <h1 className="text-2xl font-bold">Loading...</h1>;}
+  if (serverError) {return (<h1 className="text-2xl font-bold text-red-500">Error: </h1>);}
 
   return (
     <div className="container mx-auto">
@@ -24,29 +46,35 @@ const Password = () => {
       <div className="flex items-center justify-center h-screen">
         <div className={styles.glass}>
           <div className="title flex flex-col items-center">
-            <h4 className="text-5xl font-bold">Hello</h4>
+            <h4 className="text-5xl font-bold">
+              Hello {apiData?.firstName || username}
+            </h4>
             <span className="py-4 text-xl w-2/3 text-center text-gray-500">
               Enter password to continue....
             </span>
           </div>
           <form className="py-1" onSubmit={formik.handleSubmit}>
             <div className="profile flex justify-center py-4">
-              <img src={avater} alt="avatar" className={styles.profile_img} />
+              <img
+                src={apiData?.profile || avater}
+                alt="avatar"
+                className={styles.profile_img}
+              />
             </div>
             <div className="textbox flex flex-col items-center gap-6">
               <input
                 type="password"
                 placeholder="Password"
                 className={styles.textbox}
-                {...formik.getFieldProps("Password")}
+                {...formik.getFieldProps("password")}
               />
               <button className={styles.btn} type="submit">
-                Lets Go
+                Login
               </button>
             </div>
             <div className="text-center py-4">
               <span>
-              Forgot Password?{" "}
+                Forgot Password?{" "}
                 <Link className="text-gray-500" to="/recovery">
                   Recover Now
                 </Link>
