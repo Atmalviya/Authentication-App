d2a8658e170b50fc6ac27aee6e3a642912c94dbd
Atul <atulmalviyav2@gmail.com>
2024-07-25 15:43:51 +0530

    CORS issue fixed

--- server/server.js
+++ server/server.js
@@ -5,33 +5,35 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 
-//* Local imports 
+//* Local imports
 const connectDB = require("./DB/conn");
 const router = require("./router/routes");
 
 //* middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+  }));
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
 app.disable("x-powered-by");
-
 
 const PORT = process.env.PORT || 5000;
 
 // HTTP requests
 app.get("/", (req, res) => {
-    res.status(201).json("Hello World!");
+  res.status(201).json("Hello World!");
 });
 
-//* Routes 
+//* Routes
 app.use("/api", router);
 
 connectDB().then(() => {
-    try {
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        })
-    } catch (error) {
-        console.log("Something went wrong", error);
-    }
-})
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Something went wrong", error);
+  }
+});
